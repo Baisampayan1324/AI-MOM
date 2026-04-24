263c1ff1a882527460a0d82111437031e2962388
Smit Vaghasiya <smitvaghasiya11280@gmail.com>
2026-04-25 00:46:39 +0530

    Reduced refresh interval

--- twinmind/lib/useSession.ts
+++ twinmind/lib/useSession.ts
@@ -25,7 +25,7 @@ const DEFAULT_SETTINGS: Settings = {
   chatPrompt: DEFAULT_CHAT_PROMPT,
   suggestionContextWindow: 5,
   chatContextWindow: 10,
-  refreshInterval: 30,
+  refreshInterval: 10,
 };
 
 function genId(): string {
